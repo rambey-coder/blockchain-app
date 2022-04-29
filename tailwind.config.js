ac178ac9dffc9c52fb19b9c35d7620f3a9c4df90
unknown <olayiwolaagboolaakanji@gmail.com>
2022-04-30 00:29:44 +0100

    navbar done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navcolor: "rgb(18, 29, 51)",
+        textcolor: "rgb(204, 210, 222)"
+      }
+    },
   },
   plugins: [],
 }
